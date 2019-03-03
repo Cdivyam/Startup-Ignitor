ce45a179b28be76976706e652dabdf802fe83aee
Prakash <prkjha25@gmail.com>
2019-03-03 16:50:46 +0530

    blog chat video

--- routes/web.php
+++ routes/web.php
@@ -81,7 +81,7 @@ Route::get('/list_mentors','StartupController@list_mentors');
 // Route::get('/admin/stakeholder-inital','StakeholderController@index');
 Route::post('/admin/reglvl1', 'StakeholderController@store');
 //abhishek
-<<<<<<< Updated upstream
+
 Route::get('/dashboard','StartupController@dashboard')->name('startup.auth');
 
 Route::get('/dashboard','StartupController@dashboard')->name('startup.auth');
